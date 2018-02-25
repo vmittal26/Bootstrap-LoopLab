00fa3abbe589175a40437f60d0c5ce3521d451d7
vaibhavm <vaibhavmittal.26@gmail.com>
2018-02-25 14:48:46 +0530

    home section

---
+++ gulpfile_sample.js
@@ -0,0 +1,48 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+
+//Compile Sass & Inject in Browser
+
+gulp.task('sass',function(){
+    return gulp.src(['node_modules/bootstrap/scss/bootstrap.scss','./src/scss/*.scss'])
+    .pipe(sass())
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
+});
+
+//Move javascript files to src/js
+gulp.task('js', function(){
+    return gulp.src(['./node_modules/bootstrap/dist/js/bootstrap.min.js','./node_modules/jquery/dist/jquery.min.js','./node_modules/popper.js/dist/umd/popper.min.js'])
+      .pipe(gulp.dest("src/js"))
+      .pipe(browserSync.stream());
+  });
+
+//Watch Sass and Server
+gulp.task('serve',['sass'],function(){
+    browserSync.init({
+        server:"./src"
+    });
+
+    gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss','./scss/*.scss'],['sass']);
+    gulp.watch("src/*html").on('change',browserSync.reload);
+});
+
+//Move fonts to src
+gulp.task('fonts',function(){
+    return gulp.src('./node_modules/font-awesome/fonts/*')
+    .pipe(gulp.dest("src/fonts"));
+});
+
+//Move fonts awesome css to src/css
+gulp.task('fa',function(){
+    return gulp.src('./node_modules/font-awesome/css/font-awesome.min.css')
+    .pipe(gulp.dest("src/css"));
+});
+
+gulp.task('default',['js','serve','fonts','fa'])
+
+
+
+
+
